8ec8ed5dd18df262655c88c7b26d12993558a1b8
Munifconcept <nwokom9@gmail.com>
2020-07-04 00:03:22 +0100

    added some transition to the body

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const toggles=document.querySelector(".toggle-rect");
 const body = document.querySelector("body");
+const big = document.querySelector("html")
 // var swit = document.getElementsByClassName("span");
 
 toggles.addEventListener("click", function(){
     body.classList.toggle("light");
     toggles.classList.toggle("moving");
+    big.classList.toggle("transition")
 
 });
